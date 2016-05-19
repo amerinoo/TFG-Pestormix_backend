2c57a0a068c380a70d7ed3f2c44f3060981d8efd
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-05-20 01:16:02 +0200

    getById

--- server.js
+++ server.js
@@ -32,4 +32,19 @@ app.get('/db', function (request, response) {
             }
         });
     });
-})
+});
+app.get('/db/:id', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+        client.query('SELECT * FROM test_table WHERE id=' + request.params.id, function (err, result) {
+            done();
+            if (err) {
+                console.error(err);
+                response.send("Error " + err);
+            }
+            else {
+                response.json(result.rows);
+                //response.render('pages/db', {results: result.rows});
+            }
+        });
+    });
+});
