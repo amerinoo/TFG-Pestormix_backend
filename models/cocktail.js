6f6481fd0265ceb891f54b99b0ce7b4beed58334
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-05-20 02:05:54 +0200

    Try to Post a cocktail yet

--- models/cocktail.js
+++ models/cocktail.js
@@ -2,30 +2,25 @@ module.exports = function(sequelize, DataTypes) {
     return sequelize.define('cocktail', {
         name: {
             type: DataTypes.STRING,
-            allowNull: false,
             validate: {
                 len: [1, 250]
             }
         },
         userId: {
             type: DataTypes.STRING,
-            allowNull: false,
             validate: {
                 len: [1, 250]
             }
         },
         description: {
-            type: DataTypes.STRING,
-            allowNull: false
+            type: DataTypes.STRING
         },
         alcohol: {
             type: DataTypes.BOOLEAN,
-            allowNull: false,
             defaultValue: false
         },
         drinks: {
-            type: DataTypes.STRING,
-            allowNull: false
+            type: DataTypes.STRING
         }
     });
 };
